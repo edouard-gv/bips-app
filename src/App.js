4b865e9c46b713301b555a6b80b7da2c77c3c42d
Édouard Gomez-Vaëz <edouard@gomez-vaez.com>
2023-05-04 16:27:28 +0200

    Première version générée par GPT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const API_URL = 'https://your-api-gateway-url/your-api-path';
 
 function App() {
+  const [pseudo, setPseudo] = useState('');
+  const [location, setLocation] = useState('');
+  const [statusCode, setStatusCode] = useState('');
+  const [bips, setBips] = useState([]);
+
+  const handleSubmit = async () => {
+    const data = {
+      pseudo,
+      location,
+      status_code: statusCode,
+    };
+    await axios.post(API_URL, data);
+    const response = await axios.get(API_URL, { params: { location } });
+    setBips(response.data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Bips App</h1>
+      <input
+        type="text"
+        placeholder="Pseudo"
+        value={pseudo}
+        onChange={(e) => setPseudo(e.target.value)}
+      />
+      <select value={location} onChange={(e) => setLocation(e.target.value)}>
+        <option value="">Select Location</option>
+        <option value="chez mon client">Chez mon client</option>
+        <option value="au siège">Au siège</option>
+      </select>
+      <select value={statusCode} onChange={(e) => setStatusCode(e.target.value)}>
+        <option value="">Select Status Code</option>
+        <option value="je veux un café">Je veux un café</option>
+        <option value="chouette j'ai du boulot">Chouette j'ai du boulot</option>
+      </select>
+      <button onClick={handleSubmit}>Submit</button>
+      <h2>Bips</h2>
+      <ul>
+        {bips.map((bip, index) => (
+          <li key={index}>
+            {bip.pseudo} - {bip.status_code} - {bip.timestamp}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
